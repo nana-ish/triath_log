366f13c22c045d441e107dd24e521eec93d92bf2
nana-ish <nanaishlaughaway@gmail.com>
2022-04-23 06:13:04 +0000

    [add][fix]adminのend_user情報

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -9,8 +9,31 @@ class Admin::EndUsersController < ApplicationController
   end
 
   def edit
+    @end_user = EndUser.find(params[:id])
   end
 
   def update
+    @end_user = EndUser.find(params[:id])
+    redirect_to admin_end_users_path, notice: 'ゲストは変更・削除できません' if @end_user.email == 'guest@example.com'
+    if @end_user.update(end_user_params)
+      redirect_to admin_end_users_path, notice: '会員情報が正常に更新されました。'
+    else
+      render edit, notice: '会員情報の更新に失敗しました。'
+    end
   end
+
+  def destroy
+    @end_user = EndUser.find(params[:id])
+    redirect_to admin_end_users_path, notice: 'ゲストは変更・削除できません' if @end_user.email == 'guest@example.com'
+    @end_user.destroy
+    redirect_to  admin_end_users_path, notice: '正常に削除されました。'
+  end
+
+  private
+
+  def end_user_params
+    params.require(:end_user).permit(:name,:email,:end_user_image,:is_deleted)
+  end
+
+
 end
